394f1b37758e89bd34389370eaa0b638247145af
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-09-04 15:12:23 +0200

    refactor

--- src/app/api/public/contracts/[contractName]/route.ts
+++ src/app/api/public/contracts/[contractName]/route.ts
@@ -1,12 +1,11 @@
-import {NextRequest, NextResponse} from "next/server";
-import {getContractsFunctions} from "@/contracts/interfaces";
-import {StatusCodes} from "http-status-codes";
-import {withDeveloperApiToken} from "@/app/middleware/withDeveloperApiToken";
+import { NextRequest, NextResponse } from "next/server";
+import { getContractsFunctions } from "@/contracts/interfaces";
+import { StatusCodes } from "http-status-codes";
+import { withDeveloperApiToken } from "@/app/middleware/withDeveloperApiToken";
 
-async function handler(req: NextRequest, { params: { contractName } }){
-   const functions = getContractsFunctions(contractName);
-
-    return NextResponse.json({functions},{status: StatusCodes.OK});
+async function handler(req: NextRequest, { params: { contractName } }) {
+  const functions = getContractsFunctions(contractName);
+  return NextResponse.json({ functions }, { status: StatusCodes.OK });
 }
 
-export const GET = withDeveloperApiToken(handler)
+export const GET = withDeveloperApiToken(handler);
