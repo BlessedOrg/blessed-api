86bcaeecdfa3fef6711471d5c084a9ca31a8d472
adiusz <leeszczuu@gmail.com>
2024-09-10 14:44:10 +0200

    handle cairo's fn object as output + refactor

--- src/utils/formatCairoFunctionResult.ts
+++ src/utils/formatCairoFunctionResult.ts
@@ -1,18 +1,22 @@
+const formatToTargetFunctionOutputType = (result, targetFunction) => {
+  if (targetFunction.outputs[0].type.includes("ContractAddress")) {
+    result = `0x${result.toString(16)}`;
+  } else {
+    result = result.toString(16);
+  }
+  return result;
+};
+
 const formatCairoFunctionResult = (result, targetFunction) => {
   if (typeof result === "bigint") {
-    if (targetFunction.outputs[0].type.includes("ContractAddress")) {
-      result = `0x${result.toString(16)}`;
-    } else {
-      result = result.toString(16);
-    }
+    result = formatToTargetFunctionOutputType(result, targetFunction);
   } else if (typeof result === "object") {
     for (const key in result) {
       if (typeof result[key] === "bigint") {
-        result[key] = result[key].toString();
+        result[key] = formatToTargetFunctionOutputType(result[key], targetFunction);
       }
     }
   }
-
   return result;
 };
 
