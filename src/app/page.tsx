8789250ad295ef58671752dcd056487d36cd742d
adiusz <leeszczuu@gmail.com>
2024-09-18 20:16:06 +0200

    remove ui, make this app only API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-import { HomeDashboard } from "@/components/homeDashboard/HomeDashboard";
-import { Navigation } from "@/components/homeDashboard/navigation/Navigation";
-import { redirect } from "next/navigation";
-import { setCookie } from "cookies-next";
-
-export default function HomePage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
-  if (!!searchParams?.token) {
-    setCookie("accessToken", `${searchParams.token}`);
-    redirect("/");
-  }
+export default function HomePage() {
   return (
-    <div className="flex w-full flex-col bg-gray-500">
-      <Navigation />
-      <HomeDashboard />
-    </div>
+    <main></main>
   );
 }
--- src/components/Card.tsx
+++
@@ -1,6 +0,0 @@
-import { DetailedHTMLProps, HTMLAttributes } from "react";
-
-export const Card = ({...props}: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>) => {
-  const { children, className, ...rest } = props;
-  return <div className={`bg-white p-6 rounded-3xl ${className ? className : ""}`} {...rest}>{children}</div>
-}
