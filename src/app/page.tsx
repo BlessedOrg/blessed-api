fc1a55029bf8af20f6cd8766fd8590c38f1c5409
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-09-19 09:56:34 +0200

    ui remove

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,3 @@
-import { HomeDashboard } from "@/components/homeDashboard/HomeDashboard";
-import { Navigation } from "@/components/homeDashboard/navigation/Navigation";
-import { redirect } from "next/navigation";
-import { setCookie } from "cookies-next";
-
-export default function HomePage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
-  if (!!searchParams?.token) {
-    setCookie("accessToken", `${searchParams.token}`);
-    redirect("/");
-  }
-  return (
-    <div className="flex w-full flex-col bg-gray-500">
-      <Navigation />
-      <HomeDashboard />
-    </div>
-  );
+export default function HomePage() {
+  return <div className="flex w-full flex-col bg-gray-500">blessedd</div>;
 }
--- src/components/appDashboard/appDashboardNav/AppSelect.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { CustomButton } from "@/components/CustomComponents";
-
-export const AppSelect = () => {
-  return <CustomButton className="!bg-white !text-black-500">Select app</CustomButton>;
-};
