b153426cacbf151e7652dc1c5a6ebd4ddd571662
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-09-19 11:21:09 +0200

    ui remove

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+          { key: "Access-Control-Allow-Origin", value: "http://localhost:3002" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           {
             key: "Access-Control-Allow-Headers",
             value:
