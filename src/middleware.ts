1fd5fe1c691a07475f7e4c25890f3b280a7da3c8
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-09-22 12:42:47 +0200

    CORS update

--- src/middleware.ts
+++ src/middleware.ts
@@ -39,7 +39,10 @@ export function middleware(request: NextRequest) {
     response.headers.set("Access-Control-Allow-Origin", origin);
     response.headers.set("Access-Control-Allow-Credentials", "true");
     response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    response.headers.set("Access-Control-Allow-Headers", "*");
+    response.headers.set(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version"
+    );
   }
 
   Object.entries(corsOptions).forEach(([key, value]) => {
