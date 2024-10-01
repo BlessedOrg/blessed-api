672f7db9ba60564e56cade81c38eb2a216501118
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-10-01 13:58:28 +0200

    developers login & verify endpoint logic merge & refactor

--- src/app/api/v1/developers/login/route.ts
+++ src/app/api/v1/developers/login/route.ts
@@ -1,4 +1,3 @@
-import { developerAccountModel } from "@/prisma/models";
 import { NextRequest, NextResponse } from "next/server";
 import { StatusCodes } from "http-status-codes";
 import { sendVerificationEmailCode } from "@/server/auth/sendVerificationEmailCode";
@@ -6,18 +5,6 @@ import { sendVerificationEmailCode } from "@/server/auth/sendVerificationEmailCo
 export async function POST(req: NextRequest) {
   const body = await req.json();
   const { email } = body;
-
-  const userData = await developerAccountModel.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (!userData) {
-    return NextResponse.json({ error: "User not found" }, {
-      status: StatusCodes.NOT_FOUND,
-    });
-  }
 
   const otpCodeSent = await sendVerificationEmailCode({
     to: email,
