f11f771017504bb7668b5f03cf325f4894762dc1
adiusz <leeszczuu@gmail.com>
2024-10-17 16:25:19 +0200

    fix ts error on capsule.ts

--- src/lib/capsule.ts
+++ src/lib/capsule.ts
@@ -45,7 +45,7 @@ export async function getCapsuleSigner(capsuleTokenVaultKey: string) {
   const account = createCapsuleViemAccount(capsuleOneTimeClient);
   const capsuleViemClient = createCapsuleViemClient(capsuleOneTimeClient, {
     chain: activeChain,
-    transport: http(rpcUrl),
+    transport: http(rpcUrl) as any,
     account
   });
 
