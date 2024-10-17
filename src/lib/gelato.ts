736e4761cf98662671f6f2dc511990eea04c229f
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-10-17 10:54:20 +0200

    test clg's

--- src/lib/gelato.ts
+++ src/lib/gelato.ts
@@ -6,13 +6,16 @@ import { StatusCodes } from "http-status-codes";
 export const gaslessTransaction = async (request: CallWithERC2771Request, capsuleTokenVaultKey: string): Promise<{ data?: { taskId?: string }, error?: any, status?: number }> => {
   try {
     const relay = new GelatoRelay();
+    console.log(1);
     const capsuleSigner = await getCapsuleSigner(capsuleTokenVaultKey);
+    console.log(2);
     const { struct, signature } = await relay.getSignatureDataERC2771(
       request,
       capsuleSigner as any,
       ERC2771Type.SponsoredCall
     );
 
+    console.log(3);
     const relayResponse = await relay.sponsoredCallERC2771WithSignature(
       struct,
       signature,
@@ -21,6 +24,7 @@ export const gaslessTransaction = async (request: CallWithERC2771Request, capsul
 
     return { data: relayResponse };
   } catch (e) {
+    console.log(e);
     return { error: JSON.stringify(e), status: StatusCodes.INTERNAL_SERVER_ERROR };
   }
 };
